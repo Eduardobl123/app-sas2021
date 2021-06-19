16dc82b4b789b0b9d49af26d42c373ff90c68115
Eduardobl123 <62029989+Eduardobl123@users.noreply.github.com>
2021-06-19 13:42:20 -0400

    Update excluir_atividade.php

--- tela_aluno/excluir_atividade.php
+++ tela_aluno/excluir_atividade.php
@@ -1,8 +1,8 @@
 <?php
-$conexao = pg_connect("host=localhost port = 5432 dbname=bd_atividades_complementares user=postgres password=1247") or
+$conexao = mysqli_connect("bd-saas.mysql.uhserver.com:3306", "eduardo_bruno", "brunosafado*2021", "bd_saas") or
 die ("Não foi possível conectar ao servidor PostGreSQL");
 
-$result = pg_query($conexao, "delete from tb_atividade where id_atividade = ".$_GET['id_atv']);
+$result = mysqli_query($conexao, "delete from tb_atividade where id_atividade = ".$_GET['id_atv']);
 
 header('Location: minhas-atividades.php');
 
